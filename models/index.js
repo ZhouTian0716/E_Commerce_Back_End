9f0aea5af2bb51845a68008225f5c0f96c3384f1
ZhouTian0716 <egglord0716@gmail.com>
2021-05-31 10:52:56 +1000

    routes pending

--- models/index.js
+++ models/index.js
@@ -4,14 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product'
+});
 module.exports = {
   Product,
   Category,
